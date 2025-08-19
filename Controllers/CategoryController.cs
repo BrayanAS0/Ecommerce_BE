94610293b0e1a77051b7406359dcdf9f9457288c
BrayanAS0 <el.el.brayan16@gmail.com>
2025-08-18 23:24:14 -0600

    NEW FEATURES ADDED
    ADDED AUTOMAPPER

---
+++ Controllers/CategoryController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiEcommerce.Controllers;
+[ApiController]
+[Route("api/[controller]")]
+    public class CategoryController:ControllerBase
+    {
+    [HttpGet]
+    public IActionResult ObtenerCategorias()
+    {
+        return Ok(new { message = "aqui va eaks categorias" });
+    }
+
+
+
+
+    }
+
