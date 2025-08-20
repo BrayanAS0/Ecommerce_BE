f706ff7cfa366dc143ee8848ff951bb3a524417c
BrayanAS0 <el.el.brayan16@gmail.com>
2025-08-19 23:52:33 -0600

    features
    added new packet for mapper

--- Program.cs
+++ Program.cs
@@ -1,11 +1,14 @@
 using ApiEcommerce.Data;
+using ApiEcommerce.Repository;
+using ApiEcommerce.Repository.IRepository;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 var dbConnectionString = builder.Configuration.GetConnectionString("DefaultConnection");//para optener la conexion
 builder.Services.AddDbContext<ApplicationDbContext>(option => option.UseSqlServer(dbConnectionString));//para conectar la base de datos con el db context
-// Add services to the container.
 
+builder.Services.AddScoped<ICategoryRepository,CategoryRepository>();
+builder.Services.AddAutoMapper(typeof(Program).Assembly);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
